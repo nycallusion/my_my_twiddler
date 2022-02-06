aabca48d7800bf823fb973dfa9cc6a873a08b598
Dave.L <dlau3335@gmail.com>
2022-02-06 16:48:14 -0500

    testing

--- my_twiddler_frontend/src/components/Home.jsx
+++ my_twiddler_frontend/src/components/Home.jsx
@@ -4,7 +4,7 @@ import TweetCard from './TweetCard';
 import '../css/layout.scss';
 
 // end point for socket io
-const ENDPOINT = 'localhost:8080'
+const ENDPOINT = 'https://www.davidcodedesign.com'
 
 
 export default function Home() {
